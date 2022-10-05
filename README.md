36e784ada80b8497ae670524ac8a13e0c51ac38f
James Sangalli <j.l.sangalli@gmail.com>
2022-10-05 14:08:05 +1100

    setup and expand readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dot-pool-selector
-Select the best nomination pools algorithmically 
+Select the best nomination pools algorithmically
+
+## Criteria
+The algorithm selects pools based on the following:
+
+- The root user is verified
+- The root user has skin in the game 
+- The pool's validators meet general standards, see more [here](https://github.com/James-Sangalli/dot-validator-selector#criteria)
+- The pool is in an open state 
+- The pool has not reached the max member count 
+- The pool has selected an optimal number of validators
---
+++ test/PoolSelector.test.js
@@ -0,0 +1,38 @@
+const { chai, expect } = require('chai');
+const { ApiPromise, WsProvider} = require("@polkadot/api");
+
+describe("ValidatorSelector functionality", () => {
+
+    before(async() => {
+
+    });
+
+    it("should only get pools where the root is verified", async() => {
+
+    });
+
+    it("should only get pools where the max member count has not been reached", async() => {
+
+    });
+
+    it("should only get pools where the root has skin in the game", async() => {
+
+    });
+
+    it("should exclude a pool with validators that don't meet the requirements", async() => {
+
+    });
+
+    it("should only get pools that are in an open state", async() => {
+
+    });
+
+    it("should only get pools with an optimal amount of validators", async() => {
+
+    });
+
+    it("should be able to check a current selection", async() => {
+
+    });
+
+});
