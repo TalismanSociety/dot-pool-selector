40e280a6f7cf608346f8ce79611d14d047f75aac
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-10-31 14:52:57 +1300

    refactor: expose validator selector

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
+import type { ApiPromise } from "@polkadot/api";
+
 export * from "./PoolSelector";
 export { default } from "./PoolSelector";
+export { defaultOptions } from "./Types";
+
+export interface ValidatorSelector {
+  new (
+    api: ApiPromise,
+    maxCommission?: number,
+    minCommission?: number,
+    minStaking?: number,
+    era?: number,
+    humanReadable?: boolean
+  ): ValidatorSelector;
+
+  getMeetsCriteriaByAccountId: (accountId: string) => Promise<boolean>;
+}
+
+export const ValidatorSelector: ValidatorSelector = require("dot-validator-selector/util/ValidatorSelector.js");
